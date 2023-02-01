b8fdb0260fac698fc1e3b4c81d35202efb014968
Fabiano Jose <fabianoj84@gmail.com>
2023-02-01 11:25:26 -0300

    News workbooks

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,22 @@
-console.log('test...')
+class Calculator {
+  constructor() {
+    this.upperValue = document.querySelector("#upper-number")
+    this.resultValue = document.querySelector("#result-number")
+    this.reset = 0
+  }
+  btnPress() {
+    console.log("apertou")
+  }
+}
+
+// Start Obj
+
+let calc = new Calculator()
+
+// Start btns
+let buttons = document.querySelectorAll(".btn")
+
+//map all buttons
+for (let i = 0; buttons.length > i; i++) {
+  buttons[i].addEventListener("click", calc.btnPress)
+}
